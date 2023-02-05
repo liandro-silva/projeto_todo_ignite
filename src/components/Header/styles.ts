2bea23a80a03bccb177466c86e2c5d118ca59a44
Liandro Silva <liandro.silva@outlook.com>
2023-02-05 16:25:11 -0300

    feat: create Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../theme';
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.base.gray[700],
+        height: 150,
+        alignItems: 'center',
+    },
+    logo: {
+        marginTop: 45
+    }
+})
