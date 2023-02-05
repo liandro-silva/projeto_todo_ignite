b41ec6f94de8a07fbd1087bfcecaec6476b80ac4
Liandro Silva <liandro.silva@outlook.com>
2023-02-05 14:39:42 -0300

    feat: add WrapperLayout into the Home and add styles to WrapperLayout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, Text, StyleSheet } from 'react-native';
+import { WrapperLayout } from './src/layouts';
+import { colors } from './src/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      <WrapperLayout style={globalStyles.container}>
+        <Text>asdasd</Text>
+      </WrapperLayout>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const globalStyles = StyleSheet.create({
   container: {
+    backgroundColor: colors.base.gray[600],
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    padding: 24
+  }
+})
