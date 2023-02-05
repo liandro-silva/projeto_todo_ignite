feaa55008163c5ddfd7592909990f821698e7448
Liandro Silva <liandro.silva@outlook.com>
2023-02-05 14:38:41 -0300

    style: add design tokens

---
+++ src/theme/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    product: {
+        ["purple-dark"]: "#5E60CE",
+        ["blue-dark"]: "#1E6F9F",
+        purple: "#8284FA",
+        blue: "#4EA8DE"
+    },
+    base: {
+        gray: {
+            700: "#0D0D0D",
+            600: "#1A1A1A",
+            500: "#262626",
+            400: "#333333",
+            300: "#808080",
+            200: "#D9D9D9",
+            100: "#F2F2F2"
+        },
+        danger: "#E25858"
+    }
+}
---
+++ src/theme/font.ts
@@ -0,0 +1,12 @@
+export const font = {
+    sizes: {
+        normal: 14,
+        large: 16,
+        small: 12,
+    },
+    fontFamily: 'Inter',
+    weights: {
+        regular: "400",
+        bold: "700"
+    }
+}
