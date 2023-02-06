6301f0db65b11fe5109293a8abe9e5f2e4437dc3
Liandro Silva <liandro.silva@outlook.com>
2023-02-05 23:10:51 -0300

    feat: add onAddNewTask prop to create a new task

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -1,17 +1,25 @@
+import React from "react";
 import { Text, TextInput, TouchableOpacity, View } from "react-native"
-
 import { styles, colors } from './styles'
+
 import Icon from '@expo/vector-icons/AntDesign';
 
-export const TaskForm = () => {
+type TaskFormProps = {
+    onAddNewTask: (title: string) => void;
+}
+
+export const TaskForm: React.FC<TaskFormProps> = ({ onAddNewTask }) => {
+    const [title, setTitle] = React.useState('');
     return (
         <View style={styles.container}>
             <TextInput 
                 style={styles.textInput}
                 placeholder="Adicione uma nova tarefa"
-                placeholderTextColor={colors.base.gray[300]} 
+                placeholderTextColor={colors.base.gray[300]}
+                value={title}
+                onChangeText={e => setTitle(e)} 
             />
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity style={styles.button} onPress={() => onAddNewTask(title)}>
                 <Text>
                     <Icon name="pluscircleo" size={16} color={colors.base.gray[100]} />
                 </Text>
