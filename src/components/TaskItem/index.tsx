2cd60f8d8bef5be24c1a7e1c56bdc355ef8bf6d2
Liandro Silva <liandro.silva@outlook.com>
2023-02-05 23:18:55 -0300

    feat: add completed tasks indicator

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -1,25 +1,52 @@
-import { View } from 'react-native';
+import React from 'react';
+import { FlatList, View, Text } from 'react-native';
 import { styles, colors } from './styles';
 
+import { Tasks } from '../../interfaces';
+
 import { TaskItemAnalytics } from './components/TaskItemAnalytics';
+import { TaskItemBox } from './components/TaskItemBox';
 
+export type TaskItemProps = {
+    tasks: Tasks[],
+    onRemoveTask: (id: string) => void,
+    onUpdateTask: (id: string, task: Tasks) => void
+}
 
-
-
-export const TaskItem = () => (
+export const TaskItem: React.FC<TaskItemProps> = ({ tasks, onRemoveTask, onUpdateTask }) => (
     <View style={styles.container}>
         <View style={styles.taskItemAnalyticsContainer}>
             <TaskItemAnalytics
                 title='Criadas'
-                quantity={5}
+                quantity={tasks.length}
                 titleColor={colors.product.blue}
             />
             <TaskItemAnalytics
                 title='Concluídas'
-                quantity={2}
+                quantity={tasks.filter(task => task.isCompleted === true).length}
                 titleColor={colors.product.purple}
                 alignRight
             />
         </View>
+        <FlatList 
+            data={tasks}
+            keyExtractor={item => item.uuid}
+            showsVerticalScrollIndicator={false}
+            style={{
+                height: '90%',
+            }}
+            ListEmptyComponent={() => (
+                <Text style={{ color: 'red'}}>Vazio</Text>
+            )}
+            renderItem={({ item }) => (
+                <TaskItemBox 
+                    title={item.title}
+                    id={item.uuid}
+                    isCompleted={item.isCompleted}
+                    onUpdateTask={onUpdateTask}
+                    onRemoveTask={onRemoveTask}
+                />
+            )}
+        />
     </View>
 );
